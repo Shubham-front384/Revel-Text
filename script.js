965292d47c451251cc12949a3a0f5249ac11c24b
Shubham-front384 <shubhamworkemail07@gmail.com>
2025-11-27 21:24:50 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 let p = document.querySelector("p");
 let character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 let str = p.innerText;
-let interval;
+let interval = 0;
 
 p.addEventListener("mouseenter", (e) => {
-  
-  clearInterval(interval);
-
-  interval = setInterval(() => {
+  interval = 0;
+  setInterval(() => {
     let data = str.split("").map((ele, ind) => {
+      if (ind < interval) {
+        return ele
+      }
       return character.split("")[Math.floor(Math.random() * str.length)]
     }).join("");
 
     p.innerHTML = data;
-  }, 100);
 
-  setTimeout(() => {
-    clearInterval(interval);
-    p.innerText = str;
-  }, 1000);
-  
+    interval += 0.2;
+  }, 100);
 });
